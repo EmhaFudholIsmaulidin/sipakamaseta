c870bf278430bbd2b3803cd9d2f9b3a43667e657
Emha "Fudhol" Ismaulidin <72345462+EmhaFudholIsmaulidin@users.noreply.github.com>
2023-08-22 20:37:19 +0800

    Update page.tsx

--- src/app/admin/store/page.tsx
+++ src/app/admin/store/page.tsx
@@ -18,6 +18,7 @@ export default async function Page() {
             },
         })
         revalidatePath('/admin/store')
+        revalidatePath('/store')
     }
 
     async function unpublishPost(userData: FormData) {
